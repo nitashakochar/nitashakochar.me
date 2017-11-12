9b8c2e007d89341b33ec637c19b205658e166ad9
Nitasha Kochar <nitashakochar@gmail.com>
2017-11-12 17:29:39 -0500

    footer issues

---
+++ main.js
@@ -0,0 +1,16 @@
+
+function showAbout() {
+    div = document.getElementById('about');
+    div.style.display = "flex";
+    div2 = document.getElementById('projects');
+    div2.style.display = "none";
+}
+
+
+
+function showProjects() {
+    div = document.getElementById('projects');
+    div.style.display = "flex";
+    div2 = document.getElementById('about');
+    div2.style.display = "none";
+}
