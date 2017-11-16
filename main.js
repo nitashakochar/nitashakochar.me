2b63741e168d053a8bc2ec424270fd3c57b924e2
Nitasha Kochar <nitashakochar@gmail.com>
2017-11-15 23:07:01 -0500

    mobile responsive

--- main.js
+++ main.js
@@ -6,9 +6,14 @@ function showAbout() {
     div2.style.display = "none";
 }
 
-
-
 function showProjects() {
+    div = document.getElementById('projects');
+    div.style.display = "flex";
+    div2 = document.getElementById('about');
+    div2.style.display = "none";
+}
+
+function showProjects2() {
     div = document.getElementById('projects');
     div.style.display = "flex";
     div2 = document.getElementById('about');
