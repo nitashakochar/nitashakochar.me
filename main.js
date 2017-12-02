bbbd66d893fa2d31786bb8f8afd12f122df31e51
Nitasha Kochar <nitashakochar@gmail.com>
2017-12-02 16:43:26 -0500

    added new things

--- main.js
+++ main.js
@@ -1,78 +1,12 @@
+//https://stackoverflow.com/questions/26027787/simplify-this-javascript-for-show-one-hide-rest
 
-function showAbout() {
-    div = document.getElementById('about');
-    div.style.display = "flex";
-    div2 = document.getElementById('projects');
-    div2.style.display = "none";
-    div3 = document.getElementById('gallery');
-    div3.style.display = "none";
-    div4 = document.getElementById('liberopage');
-    div4.style.display = "none";
-    div5 = document.getElementById('miscpage');
-    div5.style.display = "none";
-}
-
-function showProjects() {
-    div = document.getElementById('projects');
-    div.style.display = "flex";
-    div2 = document.getElementById('about');
-    div2.style.display = "none";
-    div3 = document.getElementById('gallery');
-    div3.style.display = "none";
-    div4 = document.getElementById('liberopage');
-    div4.style.display = "none";
-    div5 = document.getElementById('miscpage');
-    div5.style.display = "none";
-}
-
-function showProjects2() {
-    div = document.getElementById('projects');
-    div.style.display = "flex";
-    div2 = document.getElementById('about');
-    div2.style.display = "none";
-    div3 = document.getElementById('gallery');
-    div3.style.display = "none";
-    div4 = document.getElementById('liberopage');
-    div4.style.display = "none";
-    div5 = document.getElementById('miscpage');
-    div5.style.display = "none";
-}
-
-function showGallery() {
-    div = document.getElementById('gallery');
-    div.style.display = "flex";
-    div2 = document.getElementById('about');
-    div2.style.display = "none";
-    div3 = document.getElementById('projects');
-    div3.style.display = "none";
-    div4 = document.getElementById('liberopage');
-    div4.style.display = "none";
-    div5 = document.getElementById('miscpage');
-    div5.style.display = "none";
-}
-
-function showLibero() {
-  div = document.getElementById('liberopage');
-  div.style.display = "flex";
-  div2 = document.getElementById('about');
-  div2.style.display = "none";
-  div3 = document.getElementById('projects');
-  div3.style.display = "none";
-  div4 = document.getElementById('gallery');
-  div4.style.display = "none";
-  div5 = document.getElementById('miscpage');
-  div5.style.display = "none";
-}
-
-function showMisc() {
-  div = document.getElementById('miscpage');
-  div.style.display = "flex";
-  div2 = document.getElementById('about');
-  div2.style.display = "none";
-  div3 = document.getElementById('projects');
-  div3.style.display = "none";
-  div4 = document.getElementById('gallery');
-  div4.style.display = "none";
-  div5 = document.getElementById('liberopage');
-  div6.style.display = "none";
-}
+$(document).ready(function(){
+ $(".navigate").click(function() {
+      // Hides all images.
+      $(".flex-box").hide();
+      // Shows appropriate one.
+      var imageid = $(this).data("imageid");
+       $(".flex-box[data-imageid="+imageid+"]").show().css('display', 'flex');
+       console.log( $(".flex-box[data-imageid="+imageid+"]"))
+   });
+});
