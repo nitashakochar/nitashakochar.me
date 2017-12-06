d02a64ff1dd20bee503ef7f180d95e50aaa5d25a
Nitasha Kochar <nitashakochar@gmail.com>
2017-12-05 23:51:33 -0500

    added lots of things

--- main.js
+++ main.js
@@ -18,3 +18,22 @@ $(document).ready(function(){
    $( this ).siblings().addClass("unselected").siblings(".selected").removeClass("unselected");
    });
 });
+
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("scroll-up").style.display = "block";
+        console.log("scrolling down")
+    } else {
+        document.getElementById("scroll-up").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
