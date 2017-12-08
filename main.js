31e24daa6d004af1b9a53f101c5ec468a0c3ebe8
Nitasha Kochar <nitashakochar@gmail.com>
2017-12-08 15:25:50 -0500

    responsive

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 //https://stackoverflow.com/questions/26027787/simplify-this-javascript-for-show-one-hide-rest
 
 $(document).ready(function(){
+   $('html').animate({scrollTop: 0}, 200);
+   console.log("top");
  $(".navigate").click(function() {
       // Hides all images.
       $(".flex-box").hide();
       // Shows appropriate one.
       var imageid = $(this).data("imageid");
        $(".flex-box[data-imageid="+imageid+"]").show().css('display', 'flex');
-       console.log( $(".flex-box[data-imageid="+imageid+"]"))
+       console.log( $(".flex-box[data-imageid="+imageid+"]"));
+       $('html').animate({scrollTop: 0}, 200);
+       console.log("top");
    });
 
 });
@@ -20,6 +24,7 @@ $(document).ready(function(){
 });
 
 
+
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("scroll-up").style.display = "block";
@@ -30,10 +35,17 @@ function scrollFunction() {
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+// function topFunction() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
+// }
+
+$(document).ready(function(){
+   $("#scroll-up").click(function(){
+      console.log("scroll to top")
+   $('html').animate({scrollTop: 0}, 400, 'linear');
+});
+});
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
