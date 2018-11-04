dee4bb9b2e4560a3875ecc96b38c161fb910a678
Nitasha Kochar <nitashakochar@gmail.com>
2018-11-04 14:01:55 -0500

    new nav bar

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 //https://stackoverflow.com/questions/26027787/simplify-this-javascript-for-show-one-hide-rest
 
 
-$(document).ready(function(){
-   $('html').animate({scrollTop: 0}, 200);
-   console.log("top");
- $(".navigate").click(function() {
-      // Hides all images.
-      $(".flex-box").hide();
-      // Shows appropriate one.
-      var imageid = $(this).data("imageid");
-       $(".flex-box[data-imageid="+imageid+"]").show().css('display', 'flex');
-       console.log( $(".flex-box[data-imageid="+imageid+"]"));
-       $('html').animate({scrollTop: 0}, 200);
-       console.log("top");
-   });
-});
+// $(document).ready(function(){
+//    $('html').animate({scrollTop: 0}, 200);
+//    console.log("top");
+//  $(".navigate").click(function() {
+//       // Hides all images.
+//       $(".flex-box").hide();
+//       // Shows appropriate one.
+//       var imageid = $(this).data("imageid");
+//        $(".flex-box[data-imageid="+imageid+"]").show().css('display', 'flex');
+//        console.log( $(".flex-box[data-imageid="+imageid+"]"));
+//        $('html').animate({scrollTop: 0}, 200);
+//        console.log("top");
+//    });
+// });
 
 // $(document).ready(function(){
 //    $("nav li").click(function() {
@@ -33,12 +33,16 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
+  if (n > x.length) {
+    slideIndex = 1
   }
-  x[slideIndex-1].style.display = "block";
+  if (n < 1) {
+    slideIndex = x.length
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
 }
 
 
@@ -48,51 +52,57 @@ showDivs2(slideIndex);
 
 function plusDivs2(n) {
   showDivs2(slideIndex += n);
-    console.log(slideIndex);
+  console.log(slideIndex);
 }
 
 function showDivs2(n) {
   var i;
   var x = document.getElementsByClassName("mySlides2");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
+  if (n > x.length) {
+    slideIndex = 1
   }
-  x[slideIndex-1].style.display = "block";
+  if (n < 1) {
+    slideIndex = x.length
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
 }
 
 
 
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("scroll-up").style.display = "block";
-        console.log("scrolling down")
-    } else if(window.pageYOffset > 20 || document.documentElement.scrollTop > 20){
-      document.getElementById("scroll-up").style.display = "block";
-      console.log("scrolling down")
-    } else {
-        document.getElementById("scroll-up").style.display = "none";
-    }
-}
-
-//When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
-if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {
-            window.scrollTo(200,100) // first value for left offset, second value for top offset
-}else{
-  $(document).ready(function(){
-     $("#scroll-up").click(function(){
-        console.log("scroll to top")
-     $('html').animate({scrollTop: 0}, 400, 'linear');
-  });
-  });
-}
+//
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("scroll-up").style.display = "block";
+//     console.log("scrolling down")
+//   } else if (window.pageYOffset > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("scroll-up").style.display = "block";
+//     console.log("scrolling down")
+//   } else {
+//     document.getElementById("scroll-up").style.display = "none";
+//   }
+// }
+//
+// //When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
+//
+// if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {
+//   window.scrollTo(200, 100) // first value for left offset, second value for top offset
+// } else {
+//   $(document).ready(function() {
+//     $("#scroll-up").click(function() {
+//       console.log("scroll to top")
+//       $('html').animate({
+//         scrollTop: 0
+//       }, 400, 'linear');
+//     });
+//   });
+// }
 
 // $(document).ready(function(){
 //    $("#scroll-up").click(function(){
@@ -102,4 +112,6 @@ if (navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/)) {
 // });
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {
+//   scrollFunction()
+// };
